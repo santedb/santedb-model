8792bfd3edfc3bb5df72a5cace3d080080b87b18
Justin Fyfe <justin@fyfesoftware.ca>
2018-08-06 15:15:55 -0400

    Created interface placeholder for IQuerFilterFunction

---
+++ SanteDB.Core.Model/Query/IQueryFilterExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Model.Query
+{
+    /// <summary>
+    /// Represents a query filter extension
+    /// </summary>
+    public interface IQueryFilterExtension
+    {
+
+        /// <summary>
+        /// Gets the name of the extension
+        /// </summary>
+        String Name { get; }
+
+        /// <summary>
+        /// Construct the expression from the parameters on the query string
+        /// </summary>
+        /// <param name="scope">The scope of the current property</param>
+        /// <param name="parms">The parameters on the query string</param>
+        /// <param name="operand">The operand</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Basically this will take seomthing like <code>dateOfBirth=:(diff|&lt;=3w)2018-01-01</code> and
+        /// turn it into <code>o.DateOfBirth.Diff("2018-01-01", "w") &lt;= 3</code>
+        /// </remarks>
+        BinaryExpression Compose(Expression scope, String[] parms, Object operand);
+
+        /// <summary>
+        /// Allows the filter extension to detect if it is present for 
+        /// </summary>
+        /// <param name="expression">The binary expression to detect if this filter extension has been generated on</param>
+        /// <returns>True if the filter extension detects its own presence on the expression</returns>
+        bool Detect(BinaryExpression expression);
+
+        /// <summary>
+        /// De-compose the LINQ expression to a string representation of the function
+        /// </summary>
+        /// <param name="expression">The expression to decompose</param>
+        /// <returns>The query data for the binary expression</returns>
+        KeyValuePair<String, Object> DeCompose(BinaryExpression expression);
+    }
+}
---
+++ SanteDB.Core.Model/Query/QueryFilterExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Model.Query
+{
+    /// <summary>
+    /// Gets or sets the query filter extensions on the application context
+    /// </summary>
+    public static class QueryFilterExtensions
+    {
+
+        // The extension methods
+        private static Dictionary<String, MethodInfo> s_extensionMethods = new Dictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// Create filter extensions
+        /// </summary>
+        static QueryFilterExtensions()
+        {
+            AddExtendedFilter(typeof(QueryFilterExtensions).GetRuntimeMethod(nameof(First), new Type[] { typeof(String), typeof(int), typeof(String) }));
+            AddExtendedFilter(typeof(QueryFilterExtensions).GetRuntimeMethod(nameof(Last), new Type[] { typeof(String), typeof(int), typeof(String) }));
+        }
+
+        /// <summary>
+        /// Add an extended filter to the list
+        /// </summary>
+        /// <param name="method">The extension method that should be added to the filter</param>
+        public static void AddExtendedFilter(MethodInfo method)
+        {
+            // Validate
+            if (method.GetParameters().Count() == 0)
+                throw new ArgumentOutOfRangeException("Extension method must have one parameter");
+
+            string methName = method.Name.ToLower();
+
+            lock (s_extensionMethods)
+                if (!s_extensionMethods.ContainsKey(methName))
+                    s_extensionMethods.Add(methName, method);
+
+        }
+
+        /// <summary>
+        /// Get an extended filter name
+        /// </summary>
+        public static MethodInfo GetExtendedFilter(String name)
+        {
+            MethodInfo retVal = null;
+            s_extensionMethods.TryGetValue(name, out retVal);
+            return retVal;
+        }
+
+        /// <summary>
+        /// Begins with 
+        /// </summary>
+        public static bool First(this String me, int length, String compare)
+        {
+            return me.Substring(length) == compare.Substring(length);
+        }
+
+        /// <summary>
+        /// Ends with
+        /// </summary>
+        public static bool Last(this String me, int length, String compare)
+        {
+            return me.Substring(me.Length - length, length) == compare.Substring(compare.Length - length, length);
+        }
+    }
+}
