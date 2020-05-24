54802fd63f6186c5b3d7703bac083af49166ea45
Justin Fyfe <justin@fyfesoftware.ca>
2020-05-24 11:29:08 -0400

    Append missing files

---
+++ SanteDB.Core.Model/Attributes/ClassConceptKeyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Core.Model.Attributes
+{
+    /// <summary>
+    /// Maps a class concept key to this type
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ClassConceptKeyAttribute : Attribute
+    {
+
+        /// <summary>
+        /// Gets or set teh class concept key
+        /// </summary>
+        public String ClassConcept { get; set; }
+
+        /// <summary>
+        /// Creates a new class concept key attribute
+        /// </summary>
+        public ClassConceptKeyAttribute(String classConceptGuid)
+        {
+            this.ClassConcept = classConceptGuid;
+        }
+    }
+}
