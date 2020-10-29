7786f9be9713ab4fc822cb6ead443710710d96c6
Justin Fyfe <justin@fyfesoftware.ca>
2020-10-29 18:04:25 -0400

    added has template interface

---
+++ SanteDB.Core.Model/Interfaces/IHasTemplate.cs
@@ -0,0 +1,20 @@
+﻿using SanteDB.Core.Model.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Core.Model.Interfaces
+{
+    /// <summary>
+    /// Indicates an object has a template
+    /// </summary>
+    public interface IHasTemplate
+    {
+
+        /// <summary>
+        /// Get the tamplate for the object
+        /// </summary>
+        TemplateDefinition Template { get; set; }
+
+    }
+}
