0be57620ea6563f910b6fdd496a8766b357543ad
Justin Fyfe <justin@fyfesoftware.ca>
2020-11-15 16:58:48 -0500

    Updated the reset password challenge questions. Updated the ORM to support lower case extension. Updated the granting for override claims

---
+++ SanteDB.Core.Model/Query/FilterExtension/IgnoreCaseQueryFilterExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace SanteDB.Core.Model.Query.FilterExtension
+{
+    /// <summary>
+    /// Represents the lower query filter extension
+    /// </summary>
+    public class IgnoreCaseQueryFilterExtension : IQueryFilterExtension
+    {
+
+        /// <summary>
+        /// Gets the lower extension name
+        /// </summary>
+        public string Name => "nocase";
+
+        /// <summary>
+        /// The actual method
+        /// </summary>
+        public MethodInfo ExtensionMethod => typeof(String).GetRuntimeMethod(nameof(String.ToLowerInvariant), Type.EmptyTypes);
+
+        /// <summary>
+        /// Compose the expression
+        /// </summary>
+        public BinaryExpression Compose(Expression scope, ExpressionType comparison, Expression valueExpression, Expression[] parms)
+        {
+            return Expression.MakeBinary(comparison,
+                Expression.Call(scope, this.ExtensionMethod), Expression.Call(valueExpression, this.ExtensionMethod));
+        }
+    }
+}
