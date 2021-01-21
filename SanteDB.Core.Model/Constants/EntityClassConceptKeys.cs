30b141ebe2f2c4eb7ef159f5277a1bab437e4c1a
Justin Fyfe <justin@fyfesoftware.ca>
2021-01-21 16:21:40 -0500

    Correct spelling

--- SanteDB.Core.Model/Constants/EntityClassConceptKeys.cs
+++ SanteDB.Core.Model/Constants/EntityClassConceptKeys.cs
@@ -58,7 +58,7 @@ namespace SanteDB.Core.Model.Constants
         /// <summary>
         /// Represents a precinct or sub-division of a city such as a burrogh
         /// </summary>
-        public const string PrecinctOrBurrogh = "ACAFE0F2-E209-43BB-8633-3665FD7C90BA";
+        public const string PrecinctOrBorough = "ACAFE0F2-E209-43BB-8633-3665FD7C90BA";
 
         /// <summary>
         /// Device
@@ -241,7 +241,7 @@ namespace SanteDB.Core.Model.Constants
         /// <summary>
         /// Represents a precinct or sub-division of a city such as a burrogh
         /// </summary>
-        public static readonly Guid PrecinctOrBurrogh = Guid.Parse("ACAFE0F2-E209-43BB-8633-3665FD7C90BA");
+        public static readonly Guid PrecinctOrBorough = Guid.Parse("ACAFE0F2-E209-43BB-8633-3665FD7C90BA");
 
     }
 }
