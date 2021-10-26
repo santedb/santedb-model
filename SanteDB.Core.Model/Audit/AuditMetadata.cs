d70644116a2e1a565cd76b31b1ae71f0978d1f27
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-26 12:43:29 -0400

    Updated metadata to include fwd info

--- SanteDB.Core.Model/Audit/AuditMetadata.cs
+++ SanteDB.Core.Model/Audit/AuditMetadata.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using Newtonsoft.Json;
 using SanteDB.Core.Model.Attributes;
 using System.Xml.Serialization;
@@ -35,71 +36,85 @@ namespace SanteDB.Core.Auditing
         /// </summary>
         [XmlEnum("pid")]
         PID,
+
         /// <summary>
         /// The object is a process name
         /// </summary>
         [XmlEnum("process")]
         ProcessName,
+
         /// <summary>
-        /// Identifies the remote host
+        /// Identifies the remote host as an IP address
         /// </summary>
         [XmlEnum("remoteHost")]
         RemoteHost,
+
         /// <summary>
         /// Identifies the remote endpoint
         /// </summary>
-        [XmlEnum("remoteEp")]
-        RemoteEndpoint,
+        [XmlEnum("proxy")]
+        ForwardInformation,
+
         /// <summary>
         /// Identifies the local endpoint
         /// </summary>
         [XmlEnum("localEp")]
         LocalEndpoint,
+
         /// <summary>
         /// Identifies the time a batch was submitted
         /// </summary>
         [XmlEnum("submission_time")]
         SubmissionTime,
+
         /// <summary>
         /// Identifies if an object was in original format
         /// </summary>
         [XmlEnum("format")]
         OriginalFormat,
+
         /// <summary>
         /// Identifies the status of the object
         /// </summary>
         [XmlEnum("status")]
         SubmissionStatus,
+
         /// <summary>
         /// Identifies the priority of the object
         /// </summary>
         [XmlEnum("priority")]
         Priority,
+
         /// <summary>
         /// Identifies the object classification
         /// </summary>
         [XmlEnum("classification")]
         Classification,
+
         /// <summary>
         /// Identifies the object as a session identifier
         /// </summary>
         [XmlEnum("sessionId")]
         SessionId,
+
         /// <summary>
         /// Identifies the object as an enterprise site identifier
         /// </summary>
         [XmlEnum("enterpriseSiteId")]
         EnterpriseSiteID,
+
         /// <summary>
         /// Identifies the object's metadata as the source
         /// </summary>
         [XmlEnum("auditSourceId")]
         AuditSourceID,
+
         /// <summary>
         /// Identifies the source type
         /// </summary>
         [XmlEnum("auditSourceType")]
         AuditSourceType,
+
         /// <summary>
         /// Allows the correlation of data between audits
         /// </summary>
@@ -115,13 +130,11 @@ namespace SanteDB.Core.Auditing
     [Classifier(nameof(Key))]
     public class AuditMetadata
     {
-
         /// <summary>
         /// Default ctor
         /// </summary>
         public AuditMetadata()
         {
-
         }
 
         /// <summary>
@@ -144,6 +157,5 @@ namespace SanteDB.Core.Auditing
         /// </summary>
         [XmlAttribute("value"), JsonProperty("value"), QueryParameter("value")]
         public string Value { get; set; }
-
     }
 }
