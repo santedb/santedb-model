47eb5c7635251134c51a00e88ed230f69b0e2f64
Justin Fyfe <justin@fyfesoftware.ca>
2023-01-18 11:41:08 -0500

    Updated version entity data to fix compile error

--- SanteDB.Core.Model/VersionedEntityData.cs
+++ SanteDB.Core.Model/VersionedEntityData.cs
@@ -42,9 +42,7 @@ namespace SanteDB.Core.Model
     [XmlType(Namespace = "http://santedb.org/model"), JsonObject("VersionedEntityData")]
     public abstract class VersionedEntityData<THistoryModelType> : BaseEntityData, IVersionedData where THistoryModelType : VersionedEntityData<THistoryModelType>, new()
     {
-        // Previous version
-        private THistoryModelType m_previousVersion;
-
+        
         /// <summary>
         /// Creates a new versioned base data class
         /// </summary>
@@ -87,7 +85,7 @@ namespace SanteDB.Core.Model
                 return EntitySource.Current.Get<THistoryModelType>(this.Key, this.PreviousVersionKey.Value);
             }
 
-            return this.m_previousVersion;
+            return null;
         }
 
         /// <summary>
