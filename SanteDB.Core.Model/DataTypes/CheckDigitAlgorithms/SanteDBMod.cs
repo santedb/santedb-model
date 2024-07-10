78cecd45de2005b230bceb90f5dba7e10a2b9a5a
Justin Fyfe <justin@fyfesoftware.ca>
2024-07-10 14:04:11 -0400

    Updated the santedb MOD-97 algorithm. Added ISO/IEC 7064 MOD-97,10 check digit

--- SanteDB.Core.Model/DataTypes/CheckDigitAlgorithms/SanteDBMod.cs
+++ SanteDB.Core.Model/DataTypes/CheckDigitAlgorithms/SanteDBMod.cs
@@ -30,7 +30,7 @@ namespace SanteDB.Core.Model.DataTypes.CheckDigitAlgorithms
     public class Mod97CheckDigitAlgorithm : ICheckDigitAlgorithm
     {
         /// <inheritdoc/>
-        public string Name => "MOD-97 Based Algorithm";
+        public string Name => "Custom MOD-97 Based Algorithm";
 
         /// <inheritdoc/>
         public string GenerateCheckDigit(string identifierValue)
@@ -60,7 +60,7 @@ namespace SanteDB.Core.Model.DataTypes.CheckDigitAlgorithms
         private static readonly ICheckDigitAlgorithm m_mod97Algorithm = new Mod97CheckDigitAlgorithm();
 
         /// <inheritdoc/>
-        public string Name => "Inline MOD-97 Check Digit Validator";
+        public string Name => "Inline Custom MOD-97 Check Digit Validator";
 
         /// <inheritdoc/>
         public bool IsValid(IExternalIdentifier id)
@@ -72,7 +72,8 @@ namespace SanteDB.Core.Model.DataTypes.CheckDigitAlgorithms
 
             string checkDigit = id.Value.Substring(id.Value.Length - 2),
                 identifierSeed = id.Value.Substring(0, id.Value.Length - 2);
-            return !String.IsNullOrEmpty(id.CheckDigit) && id.CheckDigit.Equals(checkDigit) // If the identifier provides the check digit separately it needs to match the last two digits of the identifier
+            return 
+                (!String.IsNullOrEmpty(id.CheckDigit) && id.CheckDigit.Equals(checkDigit) || String.IsNullOrEmpty(id.CheckDigit)) // If the identifier provides the check digit separately it needs to match the last two digits of the identifier
                 && m_mod97Algorithm.ValidateCheckDigit(identifierSeed, checkDigit); // The last two digits should match the identifier seed
         }
     }
