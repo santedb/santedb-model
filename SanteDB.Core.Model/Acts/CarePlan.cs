02b5407640a5f3df69e3b71730307a09adf1e98a
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-19 16:27:13 -0400

    Removed the HarmonizeComponents as this method has been spun out into the API project

--- SanteDB.Core.Model/Acts/CarePlan.cs
+++ SanteDB.Core.Model/Acts/CarePlan.cs
@@ -110,57 +110,5 @@ namespace SanteDB.Core.Model.Acts
             return new CarePlan(p, new Act[0]) { MoodConceptKey = ActMoodKeys.Request };
         }
 
-        /// <summary>
-        /// Updates this care plan's components from another stored care plan
-        /// </summary>
-        /// <param name="other">The care plan from which the HasComponent values should be modified on</param>
-        /// <remarks>This should only be used on generated care plans and not used with stored care plans since </remarks>
-        /// <exception cref="InvalidOperationException">When this care plan is stored and not generated</exception>
-        public void HarmonizeComponents(CarePlan other)
-        {
-            if(other == null)
-            {
-                throw new ArgumentNullException(nameof(other));
-            }
-            else if(this.VersionKey.HasValue)
-            {
-                throw new InvalidOperationException(ErrorMessages.WOULD_RESULT_INVALID_STATE);
-            }
-
-            var theirComponents = other.LoadProperty(o=>o.Relationships)?.Where(r => r.RelationshipTypeKey == ActRelationshipTypeKeys.HasComponent && r.LoadProperty(o=>o.TargetAct).LoadProperty(o=>o.Protocols).Any());
-            var myComponents = this.Relationships?.Where(r => r.RelationshipTypeKey == ActRelationshipTypeKeys.HasComponent && r.TargetAct?.Protocols?.Any() == true);
-
-            if(myComponents == null || theirComponents == null)
-            {
-                return;
-            }
-
-            // The stored key for this object becomes my key
-            this.Key = other.Key;
-            // Any notes that are added are copied over
-            this.Notes = other.Notes;
-
-            foreach(var theirComp in theirComponents)
-            {
-                // Find my care plan component which maps to this sequence
-                var myComp = myComponents.FirstOrDefault(o => o.TargetAct.Protocols.Any(p => theirComp.TargetAct.Protocols.Any(tp => tp.ProtocolKey == p.ProtocolKey && tp.Sequence == p.Sequence)));
-                if(myComp == null) // We don't have this instruction - but it is stored on their careplan - so we add it
-                {
-                    this.Relationships.Add(theirComp);
-                }
-                else // We have this instruction - and the stored careplan has the instruction - we should add any data relevant to the care plan 
-                {
-                    // Any special relationships
-                    myComp.TargetAct.Relationships.AddRange(theirComp.LoadProperty(o => o.TargetAct).Relationships.Where(r => COPY_RELATIONSHIP_TYPES.Contains(r.RelationshipTypeKey)));
-                    // Ensure my keys match for updates
-                    myComp.Key = theirComp.Key;
-                    myComp.TargetActKey = myComp.TargetAct.Key = theirComp.TargetActKey;
-
-                    myComp.TargetAct.BatchOperation = DataTypes.BatchOperationType.Update;
-                    myComp.TargetAct.CopyObjectData(theirComp.TargetAct);
-                    myComp.TargetAct.Key = myComp.TargetActKey;
-                }
-            }
-        }
     }
 }
